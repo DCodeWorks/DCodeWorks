2b4b16f5dc2df38db3a78b44b468c6db6d750bbf
Daniel Hajek <daniel.hajek@hudson.com.mt>
2025-05-03 15:53:13 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://danhajek.github.io',
-	base:'DCodeWorks',
 	integrations: [mdx(), sitemap()],
 });
